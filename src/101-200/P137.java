b384f44df8fb1506dc751306b5e9b0a6c8fea46e
ozerdemburhan <46060113+ozerdemburhan@users.noreply.github.com>
2023-07-28 15:53:51 +0300

    Update P137.java

--- src/101-200/P137.java
+++ src/101-200/P137.java
@@ -7,10 +7,12 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class P137 {
     
+    private final BigInteger _4  = new BigInteger("4");
+
     private final BigInteger _20 = new BigInteger("20");
-    
+
     private final BigInteger _16 = new BigInteger("16");
-    
+
     public static void main(String[] args) {
 
         logger.info("started.");
@@ -23,32 +25,36 @@ public class P137 {
     
     private void start() {
         
-        BigInteger fp = BigInteger.ONE;
-        BigInteger f = BigInteger.TWO;
+        long f1 = 1;
+        long f2 = 1;
         
         int count = 0;
         
         while(true) {
             
-            BigInteger m = check(f);
+            Long m = check(f2);
             
             if(m != null) {
-                logger.info(++count + ": " + m.toString());
+                logger.info(++count + ": " + m);
                 
                 if(count == 15) {
                     break;
                 }
             }
             
-            BigInteger current = f;
-            f = f.add(fp);
-            fp = current;
+            long current = f2;
+            f2 = f2 + f1;
+            f1 = current;
         }
     }
     
-    private BigInteger check(BigInteger f) {
+    private Long check(long f) {
 
-        BigInteger m = _20.multiply(f.pow(2)).subtract(_16);
+        BigInteger m = _20.multiply(new BigInteger("" + f).pow(2)).subtract(_16);
+        
+        if(m.compareTo(_4) <= 0) {
+            return null;
+        }
 
         BigInteger sqrt = m.sqrt();
 
@@ -64,7 +70,7 @@ public class P137 {
         
         m = m.divide(BigInteger.TEN);
         
-        return m;
+        return m.longValue();
     }
     
 }
